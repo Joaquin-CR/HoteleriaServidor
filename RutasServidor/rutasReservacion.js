5c1699d84df0513e429b2153dc6b912d374a2212
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-12-09 19:17:18 -0600

    Se creo el archivo para la ruta de reservaciones
    De igual forma se implementaron las consultas a la base de datos

---
+++ RutasServidor/rutasReservacion.js
@@ -0,0 +1,83 @@
+const rutaReservacion = require('express').Router();
+const conexion = require('../Config/conexionMySQL');
+
+// rutaReservacion.get('/', function(req, res) {
+//     res.send("La ruta principal funciona");
+// });
+
+rutaReservacion.get('/', (req, res) => {
+    let sql = 'SELECT * FROM reservacion';
+
+    conexion.query(sql,(error, filas, campos) => {
+        if(error) throw error;
+        else
+        {
+            res.json(filas);
+        }
+    })
+});
+
+// SINGLE SELECT
+rutaReservacion.get('/:id', (req, res)=>{
+    const {id} = req.params
+    let sql = 'SELECT * FROM reservacion WHERE id_res = ?' //Consulta a la base de datos
+    conexion.query(sql,[id],(error,filas,campos)=>{
+        if(error) throw error;
+        else
+        {
+            res.json(filas)
+        }
+    })
+})
+
+//  INSERT
+rutaReservacion.post('/', (req, res)=>{
+    const{idRes, idUser, fecIn, fecOut, numA, numN, numC} = req.body;
+    const fecRes = new Date(Date.now()).toISOString();
+    console.log(fecIn);
+    console.log(fecRes);
+    let sql = `INSERT INTO reservacion(id_res, iduser_res, fecRes_res, fecIn_res, fecOut_res, numA_res, numN_res, numC_res) `+
+    `VALUES('${idRes}', '${idUser}', '${fecRes}', '${fecIn}', '${fecOut}', '${numA}', '${numN}', '${numC}')`
+    conexion.query(sql, (error, filas, campos)=>{
+        if(error) throw error;
+        else
+        {
+            res.json({status: 'reservacion agregada'})
+            // console.log("reservacion agregada");
+        }
+    })
+});
+
+// DELETE
+rutaReservacion.delete('/:id', (req, res)=>{
+    const {id} = req.params
+    let sql = `DELETE FROM reservacion WHERE id_res = '${id}'`
+    conexion.query(sql, (error, filas,campos)=>{
+        if(error) throw error;
+        else
+        {
+            res.json({status: 'reservacion eliminada'})
+        }
+    });
+});
+
+// UPDATE
+rutaReservacion.put('/:id', (req, res)=>{
+    const {id} = req.params
+    const{fecIn, fecOut, numA, numN, numC} = req.body;
+    let sql = `UPDATE reservacion SET fecIn_res = '${fecIn}',
+                fecOut_res = '${fecOut}',
+                numA_res = '${numA}',
+                numN_res = '${numN}',
+                numC_res = '${numC}'
+                WHERE id_res = '${id}'`
+    conexion.query(sql, (error, filas,campos)=>{
+        if(error) throw error;
+        else
+        {
+            res.json({status: 'reservacion actualizada'})
+        }
+    });
+});
+
+module.exports = rutaReservacion;
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hola diagnostico")
+// IMPORTAMOS LA CONEXION 
+require('./Config/conexionMySQL');
+
+// IMPORTAMOS LA LIBRERÍA/FRAMEWORK DE EXPRESS
+const express = require('express');
+
+// RECUPERAMOS EL PUERTO EN EL CUAL SE ESTÁ PUBLICANDO EL SERVIDOR
+const port = (process.env.port || 3000);
+
+// CONSTANTE DE LA APLICACIÓN COMPLETA
+const app = express();
+
+/* ********* CONFIGURAR ********* */ 
+// LE DECIMOS A LA APP QUE SE ASIGNE EN EL PUERTO QUE RECUPERAMOS
+app.set('port', port);
+
+/* ********* CONFIGURAR RUTAS ********* */ 
+app.use('/apiReservacion', require('./RutasServidor/rutasReservacion'));
+
+// INICIAR EXPRESS COMO SERVIDOR
+app.listen(app.get('port'),(error) => {
+    if(error)
+    {
+        console.log("Error al iniciar el servidor " + error);
+    }
+    else
+    {
+        console.log("Servidor iniciado en el puerto "+port);
+    }
+});
